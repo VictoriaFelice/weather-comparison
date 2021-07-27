5dfc3eb032a49d9ffc8f1554ce5d17e4f70617c8
Victoria <www.victoria@gmail.com>
2021-07-27 14:33:19 -0400

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
-# To get your API key, go to https://openweathermap.org. It it FREE but you need to register.
+# To get your API key, go to https://openweathermap.org. It is FREE, but you need to register.
 
 myWeatherApiKey = 'your-free-api-key'
